56f62ad6dd8eb2948363d56094fa728311938fe2
PedroGabrielCB <contato.pedrogabrielcb@gmail.com>
2023-12-09 14:24:13 -0300

    correção

--- Mysql/db/init.sql
+++ Mysql/db/init.sql
@@ -22,12 +22,6 @@ CREATE TABLE professor (
   PRIMARY KEY (`idprofessor`)
 );
 
--- Inserção de dados na tabela professor
-INSERT INTO professor (`idprofessor`, `nome`) VALUES (1, 'Joao Paulo');
-INSERT INTO professor (`idprofessor`, `nome`) VALUES (2, 'Gustavo');
--- Adicione mais dados conforme necessário...
-
--- Criação da Tabela curso
 CREATE TABLE `curso` (
   `idcurso` int NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
@@ -39,12 +33,6 @@ CREATE TABLE `curso` (
   CONSTRAINT `idprofessor` FOREIGN KEY (`idprofessor`) REFERENCES `professor` (`idprofessor`)
 );
 
--- Inserção de dados na tabela curso
-INSERT INTO curso (`idcurso`, `nome`,`tags`,`idprofessor`,`carga_horaria`) VALUES (1, 'Desenvolvimento Web', 'react', 1, 40);
-INSERT INTO curso (`idcurso`, `nome`,`tags`,`idprofessor`,`carga_horaria`) VALUES (2, 'Desenvolvimento Mobile', 'Flutter', 2, 100);
--- Adicione mais dados conforme necessário...
-
--- Criação da Tabela evento
 CREATE TABLE `evento` (
   `idevento` int NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
@@ -54,12 +42,6 @@ CREATE TABLE `evento` (
   CONSTRAINT `idprofessor_nova` FOREIGN KEY (`idprofessor`) REFERENCES `professor` (`idprofessor`)
 );
 
--- Inserção de dados na tabela evento
-INSERT INTO evento (`idevento`, `nome`,`idprofessor`) VALUES (1, 'Plantão Framework Backend II', 1);
-INSERT INTO evento (`idevento`, `nome`,`idprofessor`) VALUES (2, 'Plantão Desenvolvimento Mobile', 2);
--- Adicione mais dados conforme necessário...
-
--- Criação da Tabela inscricao
 CREATE TABLE `inscricao` (
   `idinscricao` int NOT NULL AUTO_INCREMENT,
   `idcurso` int DEFAULT NULL,
@@ -73,12 +55,6 @@ CREATE TABLE `inscricao` (
   CONSTRAINT `idusuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
 );
 
--- Inserção de dados na tabela inscricao
--- Certifique-se de que as referências em idcurso e idusuario existem na tabela curso e usuario, respectivamente.
-INSERT INTO inscricao (`idcurso`, `idusuario`, `progresso`, `finalizado`) VALUES (1, 1, 50, 0);
--- Adicione mais dados conforme necessário...
-
--- Criação da Tabela conteudo
 CREATE TABLE conteudo (
   `idconteudo` int NOT NULL AUTO_INCREMENT,
   `idcurso` int DEFAULT NULL,
@@ -94,12 +70,6 @@ CREATE TABLE conteudo (
   CONSTRAINT `user` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`)
 );
 
--- Inserção de dados na tabela conteudo
--- Certifique-se de que as referências em idcurso e idusuario existem na tabela curso e usuario, respectivamente.
-INSERT INTO conteudo (`idcurso`, `titulo`, `body`, `concluido`, `idusuario`, `tempo`) VALUES (1, 'Conteudo 1', 'Corpo do Conteudo 1', 1, 1, 60);
--- Adicione mais dados conforme necessário...
-
--- Criação da Tabela historico
 CREATE TABLE `historico` (
   `idhistorico` int NOT NULL,
   `idinscricao` int DEFAULT NULL,
@@ -108,6 +78,44 @@ CREATE TABLE `historico` (
   KEY `insc_idx` (`idinscricao`),
   CONSTRAINT `insc` FOREIGN KEY (`idinscricao`) REFERENCES `inscricao` (`idinscricao`)
 );
+
+
+-- Inserção de dados na tabela professor
+INSERT INTO professor (`idprofessor`, `nome`) VALUES (1, 'Joao Paulo');
+INSERT INTO professor (`idprofessor`, `nome`) VALUES (2, 'Gustavo');
+-- Adicione mais dados conforme necessário...
+
+-- Criação da Tabela curso
+
+
+-- Inserção de dados na tabela curso
+INSERT INTO curso (`idcurso`, `nome`,`tags`,`idprofessor`,`carga_horaria`) VALUES (1, 'Desenvolvimento Web', 'react', 1, 40);
+INSERT INTO curso (`idcurso`, `nome`,`tags`,`idprofessor`,`carga_horaria`) VALUES (2, 'Desenvolvimento Mobile', 'Flutter', 2, 100);
+-- Adicione mais dados conforme necessário...
+
+-- Criação da Tabela evento
+
+-- Inserção de dados na tabela evento
+INSERT INTO evento (`idevento`, `nome`,`idprofessor`) VALUES (1, 'Plantão Framework Backend II', 1);
+INSERT INTO evento (`idevento`, `nome`,`idprofessor`) VALUES (2, 'Plantão Desenvolvimento Mobile', 2);
+-- Adicione mais dados conforme necessário...
+
+-- Criação da Tabela inscricao
+
+-- Inserção de dados na tabela inscricao
+-- Certifique-se de que as referências em idcurso e idusuario existem na tabela curso e usuario, respectivamente.
+
+-- Adicione mais dados conforme necessário...
+
+-- Criação da Tabela conteudo
+
+
+-- Inserção de dados na tabela conteudo
+-- Certifique-se de que as referências em idcurso e idusuario existem na tabela curso e usuario, respectivamente.
+INSERT INTO conteudo (`idcurso`, `titulo`, `body`, `concluido`, `idusuario`, `tempo`) VALUES (1, 'Conteudo 1', 'Corpo do Conteudo 1', 1, 1, 60);
+-- Adicione mais dados conforme necessário...
+
+-- Criação da Tabela historico
 
 -- Inserção de dados na tabela historico
 -- Certifique-se de que as referências em idinscricao e idconteudo existem na tabela inscricao e conteudo, respectivamente
